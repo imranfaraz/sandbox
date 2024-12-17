9a4eb33cceddeb529ca0e7ec44e78fce29b97b40
Faraz Imran <farazimran21@icloud.com>
2024-12-17 00:37:04 -0500

    if statement check

--- sandbox.sv
+++ sandbox.sv
@@ -4,7 +4,11 @@ module sandbox #(
   input  logic                  i_clk,
   input  logic                  i_reset_n,
   input  logic [DATA_WIDTH-1:0] i_d,
-  output logic [DATA_WIDTH-1:0] o_q
+  output logic [DATA_WIDTH-1:0] o_q,
+
+  input  logic i_test_0,
+  input  logic i_test_1,
+  output logic o_test_0
 );
 
   always_ff @(posedge i_clk) begin
@@ -12,6 +16,14 @@ module sandbox #(
       o_q <= '0;
     end else begin
       o_q <= i_d;
+    end
+  end
+
+  always_comb begin
+    o_test_0 = '1;
+
+    if (i_test_0) begin
+      o_test_0 = i_test_1;
     end
   end
 
