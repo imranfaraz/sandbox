a59d6d87c08c64cf8153ba7f076abb24380d07b1
Faraz Imran <farazimran21@icloud.com>
2024-12-18 00:59:32 -0500

    changes

--- sandbox.sv
+++ sandbox.sv
@@ -1,30 +1,84 @@
 module sandbox #(
-  parameter int DATA_WIDTH = 8
+  parameter int DATA_WIDTH = 4
 ) (
-  input  logic                  i_clk,
-  input  logic                  i_reset_n,
-  input  logic [DATA_WIDTH-1:0] i_d,
-  output logic [DATA_WIDTH-1:0] o_q,
+  input logic i_clk,
+  input logic i_reset_n_sync,
+  input logic i_en,
+  input logic i_reset_n_async,
 
-  input  logic i_test_0,
-  input  logic i_test_1,
-  output logic o_test_0
+  input  logic [DATA_WIDTH-1:0] i_d,
+  output logic [DATA_WIDTH-1:0] o_q_0,
+  output logic [DATA_WIDTH-1:0] o_q_1,
+  output logic [DATA_WIDTH-1:0] o_q_2,
+
+  input  logic i_a,
+  input  logic i_b,
+  input  logic i_sel,
+  output logic o_y,
+
+  input  logic                  i_up,
+  input  logic                  i_load,
+  input  logic [DATA_WIDTH-1:0] i_load_val,
+  output logic [DATA_WIDTH-1:0] o_count
 );
 
   always_ff @(posedge i_clk) begin
-    if (!i_reset_n) begin
-      o_q <= '0;
+    o_q_0 <= i_d;
+  end
+
+  always_ff @(posedge i_clk) begin
+    if (!i_reset_n_sync) begin
+      o_q_1 <= '0;
+    end else if (i_en) begin
+      o_q_1 <= i_d;
+    end
+  end
+
+  always_ff @(posedge i_clk or negedge i_reset_n_async) begin
+    if (!i_reset_n_async) begin
+      o_q_2 <= '0;
+    end else if (i_en) begin
+      o_q_2 <= i_d;
+    end
+  end
+
+  assign o_y = i_sel ? i_b : i_a;
+
+
+
+  logic [DATA_WIDTH-1:0] count_q, count_next;
+  logic [DATA_WIDTH-1:0] load_val_q;
+
+  always_ff @(posedge i_clk) begin
+    if (!i_reset_n_sync) begin
+      count_q <= '0;
     end else begin
-      o_q <= i_d;
+      count_q <= count_next;
+    end
+  end
+
+  always_ff @(posedge i_clk) begin
+    if (!i_reset_n_sync) begin
+      load_val_q <= '0;
+    end else if (i_load) begin
+      load_val_q <= i_load_val;
     end
   end
 
   always_comb begin
-    o_test_0 = '1;
+    if (i_load) begin
+      count_next = i_load_val;
+    end else if (((count_q == '1) && i_up) || ((count_q == '0) && !i_up)) begin
+      count_next = load_val_q;
+    end else begin
+      count_next = count_q - (DATA_WIDTH)'(1'b1);
 
-    if (i_test_0) begin
-      o_test_0 = i_test_1;
+      if (i_up) begin
+        count_next = count_q + (DATA_WIDTH)'(1'b1);
+      end
     end
   end
+
+  assign o_count = count_q;
 
 endmodule
--- test_sandbox.py
+++ test_sandbox.py
@@ -1,5 +1,5 @@
 import cocotb
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import ClockCycles, Timer
 from cocotb.clock import Clock
 
 
@@ -8,13 +8,26 @@ async def test_sandbox(dut):
 
     clock = Clock(dut.i_clk, 1, units="ns")  # 1 GHz clock
     cocotb.start_soon(clock.start(start_high=False))
+    cocotb.start_soon(toggle_async_reset(dut))
 
-    dut.i_reset_n.value = 0
+    dut.i_reset_n_sync.value = 1
+    dut.i_d.value = 0xCC
     await ClockCycles(dut.i_clk, 1)
 
-    dut.i_reset_n.value = 1
+    dut.i_reset_n_sync.value = 0
+    await ClockCycles(dut.i_clk, 1)
+
+    dut.i_reset_n_sync.value = 1
     dut.i_d.value = 0xAA
     await ClockCycles(dut.i_clk, 3)
 
     dut.i_d.value = 0xFF
     await ClockCycles(dut.i_clk, 3)
+
+
+async def toggle_async_reset(dut):
+    dut.i_reset_n_async.value = 1
+    await Timer(0.75, units="ns")
+    dut.i_reset_n_async.value = 0
+    await Timer(0.5, units="ns")
+    dut.i_reset_n_async.value = 1
